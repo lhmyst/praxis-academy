aa7aacfd6351e93df300d54041024a454e3c2b53
Lukman <l.hakim.myst@gmail.com>
2019-09-05 16:26:19 +0700

    Minggu 1 hari 4

---
+++ minggu-1/04/Classes/src/main/java/Classes/Bicycle.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author lukman
+ */
+public class Bicycle {
+        
+    // the Bicycle class has
+    // three fields
+    private int cadence;
+    private int gear;
+    private int speed;
+        
+    // the Bicycle class has
+    // one constructor
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+    public Bicycle() {
+        gear = 1;
+        cadence = 10;
+        speed = 0;
+    }
+    
+        
+    // the Bicycle class has
+    // four methods
+    public int getCadence() {
+        return cadence;
+    }
+        
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public int getGear() {
+        return gear;
+    }
+        
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public int getSpeed() {
+        return speed;
+    }
+        
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) {
+        speed += increment;
+    }
+        
+}
