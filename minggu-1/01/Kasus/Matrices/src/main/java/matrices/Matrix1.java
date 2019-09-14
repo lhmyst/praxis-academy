cd7299aee21f27d74979c2e60293dc84af4843e8
lhmyst <l.hakim.myst@gmail.com>
2019-09-14 19:00:40 +0700

    Completing assignment

--- minggu-1/01/Kasus/Matrices/src/main/java/matrices/Matrix1.java
+++ minggu-1/01/Kasus/Matrices/src/main/java/matrices/Matrix1.java
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Matrices;
-
+package matrices;
 
 /**
  *
- * @author lukman
+ * @author lhmyst
  */
-public class Matrices2 {
+public class Matrix1 {
 
     /**
      * @param args the command line arguments
